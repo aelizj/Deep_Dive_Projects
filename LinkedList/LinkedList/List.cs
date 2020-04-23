6bea987b4090740cc0a1b92bbfeae340dbde29b9
Anne Jones <anne1365@outlook.com>
2020-04-23 02:40:01 -0700

    Linked list work

---
+++ LinkedList/LinkedList/List.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class List
+    {
+        public int Count { get; }
+        public string FirstValue { get; }
+        public string LastValue { get; }
+        public string NextElement { get; }
+        public ListItem Item;
+
+
+        public void AddValue(string value)
+        {
+            if (Count == 0)
+            {
+                Item = new ListItem();
+            }
+            else
+            {
+
+            }
+        }
+
+        public string GetValue(int index)
+        {
+            return "hi";
+        }
+
+        public void RemoveValue(int index)
+        {
+
+        }
+    }
+}
--- ListsHashes/LinkedList.cs
+++ ListsHashes/LinkedList.cs
@@ -1,43 +1,78 @@
 using System;
-
 namespace ListsHashes
 {
-
-public class LinkedList
-{
-    public int Count { get;}    
-    public string FirstValue { get; }
-    public string LastValue { get; }
-    public string NextElement { get; }
-    public ListItem Item;
-
-
-    public void AddValue(string value)
+    public class LinkedList
     {
-        if (Count == 0) 
+        public int Count { get;}    
+        public string FirstValue { get; }
+        public string LastValue { get; }
+        public string NextElement { get; }
+        public ListItem Item;
+
+
+        public void AddValue(string value)
         {
-            Item = new ListItem();
-        } 
-        else
+            if (Count == 0) 
+            {
+                Item = new ListItem(value);
+                Count ++;
+                FirstValue = value;
+            } 
+            else
+            {
+                Item.AddItem(value);
+                Count ++;
+                LastValue = value;
+            }
+        }
+
+        public string GetValue(int index)
+        {
+            if ((index > Count) || (index < 0))
+            {
+                return "Error: Invalid Index";
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void RemoveValue(int index)
         {
 
         }
     }
 
-    public string GetValue(int index)
+    public class ListItem
     {
-        return "hi";
+        public string Value;
+        public ListItem NextValue = null;
+
+
+        public ListItem(string value)
+        {
+            Value = value;
+        } 
+
+        public void AddItem(string value)
+        {
+
+        }
+
+        public string GetItem(int index)
+        {
+
+        }
+
+        public void RemoveItem(int index, string nextVal)
+        {
+
+        }
+
+        public LinkedList NextItem
+        {
+
+        }
     }
-
-    public void RemoveValue(int index)
-    {
-
-    }
-}
-
-public class ListItem
-{
-
-}
-
 }
