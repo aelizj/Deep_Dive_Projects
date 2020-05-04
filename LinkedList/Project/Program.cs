35f9fb5b72084712dc499966a713c25808bd0768
Anne <anne1365@outlook.com>
2020-05-04 01:52:55 -0700

    Work from today

--- LinkedList/Project/Program.cs
+++ LinkedList/Project/Program.cs
@@ -15,13 +15,37 @@ namespace Project
             linkedList.Add("four");
             linkedList.Add("five");
             linkedList.Add("six");
+            linkedList.Add("seven");
 
+            Console.WriteLine("COUNT, FIRST VALUE, NEXT VALUE, AND LAST VALUE:");
             Console.WriteLine(linkedList.Count);
             Console.WriteLine(linkedList.FirstValue);
             Console.WriteLine(linkedList.NextElement);
             Console.WriteLine(linkedList.LastValue);
+            Console.WriteLine("");
 
-            linkedList.Remove(2);
+            Console.WriteLine("ELEMENTS IN LIST:");
+            Console.WriteLine(linkedList.Get(0));
+            Console.WriteLine(linkedList.Get(1));
+            Console.WriteLine(linkedList.Get(2));
+            Console.WriteLine(linkedList.Get(3));
+            Console.WriteLine(linkedList.Get(4));
+            Console.WriteLine(linkedList.Get(5));
+            Console.WriteLine(linkedList.Get(6));
+            Console.WriteLine("");
+
+            Console.WriteLine("REMOVING ELEMENT 'five':");
+            linkedList.Remove(4);
+            Console.WriteLine(linkedList.Count);
+            Console.WriteLine(linkedList.FirstValue);
+            Console.WriteLine(linkedList.NextElement);
+            Console.WriteLine(linkedList.LastValue);
+            Console.WriteLine("");
+
+            Console.WriteLine("ELEMENTS IN LIST AFTER REMOVAL:");
+            linkedList.Get(5);
+            Console.WriteLine("");
+
         }
     }
 }
